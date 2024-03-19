d7884a1f3c69d44525dc36b7056075d42f301722
Naresh Kumar <144513215+nareshns2004@users.noreply.github.com>
2024-03-19 10:34:54 +0530

    Bubble Sorting in Java

---
+++ Algorithms/Sorting/Bubble_Sort.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Bubble_Sort
+{
+    public static void readingArray(int[] arr)
+    {
+        Scanner read = new Scanner(System.in);  
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=read.nextInt();      
+        }
+
+        read.close();
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        
+        for(int j=0;j<arr.length-1;j++)
+        {
+            for(int k=0;k<arr.length-1-j;k++)
+            {
+                if(arr[k]>arr[k+1])
+                {
+                    int temp = arr[k];
+                    arr[k] = arr[k+1];
+                    arr[k+1] = temp;
+                }
+
+            }
+        }
+
+    }
+
+    public static void preDisplayArray(int [] arr)
+    {
+        System.out.print("Array Before Sorting : ");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void postDisplayArray(int [] arr)
+    {
+        System.out.print("Array After Sorting : ");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Enter the size of the array"); 
+        Scanner read = new Scanner(System.in);                   
+        int size = read.nextInt();                          // Size of the Array to be read
+        
+        System.out.println("Enter the Numbers :");
+        int [] arr = new int[size];                         // Declaring the Array
+
+                                               
+        readingArray(arr);                                  // Reading the Array Elements
+        read.close();                                       // Closing the Scanner
+        preDisplayArray(arr);                               // Before Sorting
+        
+        bubbleSort(arr);                                    // Sorting the Array
+
+        postDisplayArray(arr);                              // After Sorting
+            
+    }
+}
