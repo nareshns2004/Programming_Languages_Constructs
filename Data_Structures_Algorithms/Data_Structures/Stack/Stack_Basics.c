919de462237b748ad828de4dd36c9e3dc6339ec9
nareshns2004 <nareshns2004@gmail.com>
2024-08-31 23:27:09 +0530

    Stack Operations Push & Pop

--- Data_Structures_Algorithms/Data_Structures/Stack/Stack_Basics.c
+++ Data_Structures_Algorithms/Data_Structures/Stack/Stack_Basics.c
@@ -32,6 +32,46 @@ void isFull(struct stack* sp)
     
 }
 
+void push(struct stack* sp, int data)          //Pushing value on Stack
+{
+    if(sp->top==sp->size-1)
+    {
+        printf("Stack is Full\n");
+        
+    }
+    else{
+        sp->top++;
+        sp->ptr[sp->top] = data;
+        
+    }
+}
+
+int pop(struct stack* sp)                      //Popping value out of Stack
+{
+    if(sp->top==-1)
+    {
+        printf("Stack is Empty\n");
+        return -1;
+    }
+    else{
+        int val = sp->ptr[sp->top];
+        sp->top--;
+
+        return val;
+    }
+}
+
+void printstack(struct stack* sp)              // Printing Stack
+{
+    int i = sp->top;
+    printf("Stack Elements are: \n");
+    while(!(i<0))
+   { 
+    printf("%d\n",sp->ptr[i]);
+    i--;
+   }
+}
+
 
 int main()
 {
@@ -42,6 +82,13 @@ int main()
 
     isEmpty(sp);
     isFull(sp);
+    push(sp,54);
+    push(sp,44);
+    push(sp,34);
+    printstack(sp);
+    int val = pop(sp);
+    printf("Pop Value: %d\n",val);
+    printstack(sp);
 
     return 0;
 }
