33551786a39082506bab30f7b782e710f42e2d7d
nareshns2004 <nareshns2004@gmail.com>
2024-10-08 23:41:18 +0530

    Squares Sorted Array

--- Procedural_Programming/C++/C++_Theory/C++_Standard_Template_Library/Vectors.cpp
+++ Procedural_Programming/C++/C++_Theory/C++_Standard_Template_Library/Vectors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-vector <int> vec = {2, 4, 6, 8};
+/*vector <int> vec = {2, 4, 6, 8};
 
 cout<<"size: "<<vec.size()<<endl;
 cout<<"capacity: "<<vec.capacity()<<endl;
@@ -16,7 +16,16 @@ cout<<"capacity: "<<vec.capacity()<<endl;
 
 vec.pop_back();
 cout<<"size: "<<vec.size()<<endl;
-cout<<"capacity: "<<vec.capacity()<<endl;
+cout<<"capacity: "<<vec.capacity()<<endl;*/
+
+vector <int> nar(0);
+nar.reserve(10);
+for(int i=0;i<11;i++)
+{
+    nar.push_back(i);
+    cout<<nar.size()<<" ";
+    cout<<nar.capacity()<<endl;
+}
 
 return 0;
 }
