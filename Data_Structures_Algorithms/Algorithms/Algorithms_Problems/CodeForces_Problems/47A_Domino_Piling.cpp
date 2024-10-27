c3c53cd0bea04b543547a980c3e3d0d7af4a7d25
nareshns2004 <nareshns2004@gmail.com>
2024-10-27 14:52:03 +0530

    47A Domino Piling

---
+++ Data_Structures_Algorithms/Algorithms/Algorithms_Problems/CodeForces_Problems/47A_Domino_Piling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m=0,n=0;
+    cin>>m>>n;         //sides of rectangle
+    int sq = 2;        //sides of a domino
+
+    cout<<m*n/2;       //no of squares needed
+
+    return 0;
+}
