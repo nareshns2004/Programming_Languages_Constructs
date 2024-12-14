179ea9d7e7f7dc510701276486c8d33a68657201
nareshns2004 <nareshns2004@gmail.com>
2024-12-14 23:31:25 +0530

    1669A_Division

--- Competitive_Programming/Competitive_Programming_Problems/CodeForces_Newbie_Problems/1669A_Division.cpp
+++ Competitive_Programming/Competitive_Programming_Problems/CodeForces_Newbie_Problems/1669A_Division.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int t;
-    cin>>t;
+int main() {
+    int t; // Number of test cases
+    cin >> t;
 
-    while(t--)
-    {
+    while (t--) {
         int rating;
-        cin>>rating;
+        cin >> rating;
 
-        if(rating<=1399){
-            cout<<"Divison "<<4<<endl;
-        }
-        else if(rating>=1400||rating<=1599){
-            cout<<"Division "<<3<<endl;
-        }
-        else if(rating>=1600||rating<=1899){
-            cout<<"Division "<<2<<endl;
-        }
-        else{
-            cout<<"Division "<<1<<endl;
+        if (rating >= 1900) {
+            cout << "Division 1" << endl;
+        } else if (rating >= 1600) {
+            cout << "Division 2" << endl;
+        } else if (rating >= 1400) {
+            cout << "Division 3" << endl;
+        } else {
+            cout << "Division 4" << endl;
         }
     }
-
 
     return 0;
 }
