0e93e924df65247eb1d4cc3f8dc8ac9f77dc9ecb
nareshns2004 <nareshns2004@gmail.com>
2024-12-17 23:03:00 +0530

    1807_A_Plus_Minus

---
+++ Competitive_Programming/Competitive_Programming_Problems/CodeForces_Newbie_Problems/1807A_Plus_Minus.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        cout<<((c==a+b)?"+":"-")<<endl;
+
+    }
+
+    return 0;
+}
