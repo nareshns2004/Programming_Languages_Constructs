cb8167f2d9728384f8a2cfef48b09bb1c41f2489
nareshns2004 <nareshns2004@gmail.com>
2025-05-17 13:29:26 +0530

    Programming_Languages_Constructs

---
+++ C/README.md
@@ -0,0 +1,3 @@
+<h3><b>Logic, Procedural, Object, Scripting, Functional, DSA, CP etc</b></h3>
+
+
--- Object_Oriented_Programming/Python/Python_Theory/Python_Introduction/Comments_Basics.py
+++
@@ -1,5 +0,0 @@
-print("Naresh", "Kumar", sep="~", end = "2004\n72")
-a = 4.7667835467579
-print(type(a))
-c = complex(8,2)
-print(c)
