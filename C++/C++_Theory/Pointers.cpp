af4f1c47630c76e1176748c7a7af2b8f3eb4c03d
nareshns2004 <nareshns2004@gmail.com>
2025-06-22 21:31:37 +0530

    C++

--- C++/C++_Theory/Pointers.cpp
+++ C++/C++_Theory/Pointers.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int getMin(int arr[], int sz){
 
+    int min = arr[0];
+
+    for(int i=0;i<sz;i++){
+        if(arr[i]<=min){
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
 
 int main(){
 
-    int n = 65;
-    char ch = 'A';
-
-    cout<<n<<endl;
-
-    void* ptr = &n;
-
-    cout<<ptr<<endl;
-
-    cout<<(*(float*)ptr)<<endl;
-
+    int arr[5] = {1,2,3,4,5};
+    cout<<getMin(arr,5)<<endl;
 
     return 0;
 
